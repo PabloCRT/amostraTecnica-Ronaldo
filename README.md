65b3a20da267be729ece26aeb601d95236a3b95e
elizasanches <rfelipinga@gmail.com>
2024-10-29 14:31:44 -0300

    Primeiro commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# amostraTecnica-Ronaldo
-Trabalho de escola envolvendo explicações leigas sobre HTML, CSS e JavaScript.
--- site-explicacao-TEC/pages/pageJS.js
+++
@@ -1,9 +0,0 @@
-var img1 = "../assets/dead1.jpg";
-var img2 = "../assets/dead2.jpg";
-
-function trocar() {
-    document.getElementById("imagem1").src = img1;
-    let aux = img1;
-    img1 = img2;
-    img2 = aux
-}
